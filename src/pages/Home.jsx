e459d8d2bf5e8a234202b553021474fe8b104809
Samuel Aliyari <samuelaliyari@outlook.de>
2023-12-05 20:54:37 +0100

    Fehler behoben

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
-
 import MovieCards from "../components/MovieCards/MoviCards";
-import Header from "../components/header/Header";
-import "./Home.scss"
+import Header from "../components/Header/Header";
+import "./Home.scss";
 import movies from "../data/Movies";
 import { useEffect, useState } from "react";
 
-
 const Home = () => {
-    const [movieData, setMovieData] = useState(movies);
+	const [movieData, setMovieData] = useState(movies);
 
-    useEffect(() => {
-        console.log("home changed")
-    }, [movieData])
+	useEffect(() => {
+		console.log("home changed");
+	}, [movieData]);
 
 	return (
 		<main>
-			<Header movieDataHome={movieData} setter = {setMovieData}/>
+			<Header
+				movieDataHome={movieData}
+				setter={setMovieData}
+			/>
 			<MovieCards movieDataHome={movieData} />
 		</main>
 	);
